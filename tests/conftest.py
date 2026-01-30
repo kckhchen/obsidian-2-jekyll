9a5ddc59e63c06df65300841e928a8d7546ce36e
Casey C. <kc.khchen@gmail.com>
2026-01-30 20:24:50 +0800

    fix: Update postify fixture to accept optional metadata

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,7 @@ import frontmatter
 
 @pytest.fixture
 def postify():
-    def _maker(content):
-        return frontmatter.Post(content)
+    def _maker(content, metadata=None):
+        return frontmatter.Post(content, **(metadata or {}))
 
     return _maker
